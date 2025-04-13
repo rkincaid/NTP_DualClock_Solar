b1322dcd5a7fbe8f6320a4dc63abe4d85095fdee
rkincaid <rhkincaid@gmail.com>
2025-04-13 15:30:51 -0700

    Update userSettings.h

--- userSettings.h
+++ userSettings.h
@@ -1,1 +1,2 @@
-
+#define NTP_SERVER "pool.ntp.org"                         // time.nist.gov, pool.ntp.org, etc
+#define TZ_RULE "PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00"  // West Coast Time, change as needed
