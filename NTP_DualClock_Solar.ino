8710b51eae6c84594edc6adb51f2cfb35361bccf
rkincaid <rhkincaid@gmail.com>
2025-04-13 13:21:55 -0700

    Update NTP_DualClock_Solar.ino
    minor formatting change

--- NTP_DualClock_Solar.ino
+++ NTP_DualClock_Solar.ino
@@ -45,7 +45,7 @@
 #include <WiFiClient.h>
 
 #define TITLE "NTP TIME"
-#define WIFI_SSID ""                             // YOUR SSID
+#define WIFI_SSID ""                         // YOUR SSID
 #define WIFI_PWD ""                          // YOUR wifi password
 #define NTP_SERVER "pool.ntp.org"                         // time.nist.gov, pool.ntp.org, etc
 #define TZ_RULE "PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00"  // West Coast Time, change as needed
