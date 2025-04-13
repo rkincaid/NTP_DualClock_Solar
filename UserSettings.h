3beba6d3b54aca65964ea63d97057d49aa678f76
rkincaid <rhkincaid@gmail.com>
2025-04-13 16:00:07 -0700

    Update UserSettings.h

--- UserSettings.h
+++ UserSettings.h
@@ -1,2 +1,2 @@
-#define NTP_SERVER "pool.ntp.org"                         // time.nist.gov, pool.ntp.org, etc
-#define TZ_RULE "PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00"  // West Coast Time, change as needed
+#define WIFI_SSID ""       // YOUR SSID
+#define WIFI_PWD ""        // YOUR wifi password
