c5bdf3549573bb34fa1e36d7dd15a8d7f33d7e34
rkincaid <rhkincaid@gmail.com>
2025-04-13 15:58:46 -0700

    Update NTP_DualClock_Solar.ino

--- NTP_DualClock_Solar.ino
+++ NTP_DualClock_Solar.ino
@@ -45,7 +45,7 @@
 #include <WiFiClient.h>
 #include <UserSettings.h>
 
-#define TITLE "CalQRP Clock"
+#define TITLE "NTP TIME"
 #define NTP_SERVER "pool.ntp.org"                         // time.nist.gov, pool.ntp.org, etc
 #define TZ_RULE "PST8PDT,M3.2.0/2:00:00,M11.1.0/2:00:00"  // West Coast Time, change as needed
 
